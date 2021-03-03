2d61504893949e1c97845e649ad72648933737a7
uzairkorai <uzairahmed097@gmail.com>
2021-03-03 17:36:55 +0500

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars')
 const logger = require('./middleware/logger')
+const members = require('./Members')
 
 const app = express();
 
 // init middleware
 // app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-// app.use(express.urlencoded({ extended: false }));
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+ title: 'Member App',
+ members
+}));
 
 // Set static folder  
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Members API Routes
-app.use('/api/members', require('./routes/api/members'))
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 
