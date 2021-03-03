e2c29b6845be12f2cfdeb34f0f4c1f5225e797b9
uzairkorai <uzairahmed097@gmail.com>
2021-03-03 15:24:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const logger = require('./middleware/logger')
+
+const app = express();
+
+// init middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// app.use(express.urlencoded({ extended: false }));
+
+// Set static folder  
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
